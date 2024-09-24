e352f9f8b29dc6809d8fa1a04f0409c75be779b1
Grimm <rafalmirkowski@gmail.com>
2024-09-24 09:00:16 +0200

    clean up code

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './create-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
-  //validation pipe is needed
+  //validation pipe is needed later
   @Post('register')
   async registerUser(@Body() user: CreateUserDto) {
     await this.userService.registerUser(user);
