9e1e64ecaae25b13aa03ec01cfa434d59de52926
Grimm <rafalmirkowski@gmail.com>
2024-09-24 07:49:33 +0200

    setup prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaKnownClientExceptions } from './common/prisma-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //prisma setup errors
+  const httpAdapter = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaKnownClientExceptions(httpAdapter));
+
   await app.listen(3000);
 }
 bootstrap();
